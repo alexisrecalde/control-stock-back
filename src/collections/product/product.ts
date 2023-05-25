f4df6a8778b760a040502401e27d783dbc1dda57
Alexis <alexis.recalde@hotmail.com>
2023-05-25 17:17:56 -0300

    first commit

---
+++ src/collections/product/product.ts
@@ -0,0 +1,39 @@
+import { CollectionConfig } from 'payload/types';
+
+const Products: CollectionConfig = {
+  slug: 'products',
+
+ 
+  access: {
+    read: () => true,
+    delete:() => true,
+  },
+  fields: [
+    {
+			name: 'name',
+      type: 'text', 
+      required: true,
+      label: "Nombre"
+		},
+		{
+			name: 'description',
+      type: 'textarea', 
+      required: true,
+      label: "Descripcion"
+    },
+    {
+			name: 'price',
+      type: 'number', 
+      required: true,
+      label: "Precio"
+    },
+    {
+			name: 'quantity',
+      type: 'number', 
+      required: true,
+      label: "Cantidad"
+		},
+  ],
+};
+
+export default Products;
